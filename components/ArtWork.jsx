8a3a4122f0c14ffc3c2602a4ea8e58e8b60d72de
oualid rahhouti <rahhoutiwalid@gmail.com>
2023-10-09 20:54:15 +0200

    fixing home page

--- components/ArtWork.jsx
+++ components/ArtWork.jsx
@@ -1,65 +1,86 @@
-import { Image, Pressable, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Image, ImageBackground, Pressable, StyleSheet, Text, View } from "react-native";
 
-export default function ArtWork()
-{
-    return (
-        <View style={styles.artwork}>
-                <Image style={styles.image} source={{
-                    uri: 'https://picsum.photos/2000/3000?random',
-                    width: 300,
-                    height: 450
-                }}/>
-                <View style={styles.dataContainer}>
-                    <View style={styles.infos}>
-                        <Text> Release Date here</Text>
-                        <Text> Title here</Text>
-                        <Text> Writer here</Text>
-                    </View>
-                    <View style={styles.buttonContainer}>
-                        <Pressable style={styles.button}>Details</Pressable>
-                    </View>
-                    
-                </View>
-                
+export default function ArtWork() {
+  return (
+    <View style={styles.artwork}>
+      <ImageBackground
+        style={styles.image}
+        resizeMode="cover"
+        source={{
+          uri: 'https://picsum.photos/600/200',
+        }}
+      >
+        <View style={styles.overlay}>
+          <View style={styles.dataContainer}>
+            <View style={styles.infos}>
+              <Text style={[styles.titleText, styles.text]}>Title here</Text>
+              <Text style={styles.text}>Release Date here</Text>
+              <Text style={styles.text}>Writer here</Text>
+            </View>
+            <View style={styles.buttonContainer}>
+              <Pressable style={styles.button}>
+                <Text style={styles.buttonText}>Details</Text>
+              </Pressable>
+            </View>
+          </View>
         </View>
-    )
+      </ImageBackground>
+    </View>
+  );
 }
 
-const styles= StyleSheet.create({
-    artwork: {
-        display: "flex",
-        flexDirection: "row",
-        backgroundColor: '#fff',
-        borderWidth: 1,
-        borderColor:'grey',
-        borderRadius: 20,
-        margin: 20
-    },
-    image:{
-        borderTopLeftRadius: 20,
-        borderBottomLeftRadius: 20
-    },
-    dataContainer: {
-        display: 'flex',
-        justifyContent: "space-between",
-        paddingVertical: 20,
-        alignItems: "center",
-        flex:1
-        },
-    infos: {
-        flex:1,
-        backgroundColor: '#bbb'
-    },
-    button: {
-        backgroundColor: 'blue',
-        color: 'white',
-        borderRadius: 3,
-        width: '10rem',
-        height: 50,
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        fontSize: 25
-    }
-    
-})
+const styles = StyleSheet.create({
+  artwork: {
+    width: 500,
+    height: 175,
+    borderRadius: 20,
+    overflow: 'hidden',
+    marginTop:10,
+    margin: 'auto',
+  },
+  image: {
+    flex: 1,
+  },
+  overlay: {
+    flex: 1,
+    justifyContent: 'flex-end', // Move dataContainer to the bottom
+    backgroundColor: 'rgba(0, 0, 0, 0.6)',
+  },
+  dataContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center", // Center the content vertically at the bottom
+    padding: 10,
+  },
+  infos: {
+    flex: 1,
+  },
+  text: {
+    color: 'white',
+  },
+  titleText: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  buttonContainer: {
+    marginLeft: 10,
+  },
+  button: {
+    backgroundColor: 'black',
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    shadowColor: 'white',
+    shadowOpacity: 0.7,
+    shadowRadius: 30,
+    width:100,
+    height:40
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+  },
+});
